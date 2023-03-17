8587db8fd546c1fe6e6a72dd5a7ddc87c4739d1a
Maryem <maryem.khaoua@gmail.com>
2023-03-17 06:55:46 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,11 @@
 int main(void)
 {
 	char alpHA;
-	
+
 	for (alpHA = 'a'; alpHA <= 'z'; alpHA++)
-	{
 		putchar(alpHA);
-	}
 	for (alpHA = 'A'; alpHA <= 'Z'; alpHA++)
-	{
 		putchar(alpHA);
-	}
 	putchar('\n');
-	return (0); }
+	return (0);
+}
