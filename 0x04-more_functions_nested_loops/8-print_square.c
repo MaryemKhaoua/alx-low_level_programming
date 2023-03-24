e6b87582b061e046341645846f8fb7c392e05b23
Maryem <maryem.khaoua@gmail.com>
2023-03-24 07:44:15 -0700

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -5,6 +5,8 @@
  */
 void print_square(int size)
 {
+	int i, j;
+
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
@@ -14,6 +16,7 @@ void print_square(int size)
 				_putchar('#');
 			}
 			_putchar('\n');
+		}
 	}
 	else
 		_putchar('\n');
