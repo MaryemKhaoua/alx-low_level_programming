250dc5ed722778244d925a8b7fe11c04593657ef
Maryem <maryem.khaoua@gmail.com>
2023-03-24 17:20:47 -0700

    The prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print largest prime factor of 612852475143
+ * Return: 0 on success
+ */
+int main()
+{
+	unsigned long n = 612852475143;
+	unsigned long i = 2;
+	unsigned long largest_prime_factor = 0;
+
+	while (i * i <= n) {
+		if (n % i == 0) {
+			n /= i;
+			largest_prime_factor = i;
+		} else {
+			i++;
+		}
+	}
+	if (n > largest_prime_factor) {
+		largest_prime_factor = n;
+	}
+	printf("%ld\n", largest_prime_factor);
+	return 0;
+}
+
