145a8c24e7aaacfee8ab6624cfc1ec7f39604864
Maryem <maryem.khaoua@gmail.com>
2023-03-27 17:34:19 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - Entry Function
+ * @dest: char
+ * @src: char
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
