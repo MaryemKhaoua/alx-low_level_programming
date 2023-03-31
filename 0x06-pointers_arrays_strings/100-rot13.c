ddb442b92f8a64eb7d75c2dcf8a3a0d9353cf02f
Maryem <maryem.khaoua@gmail.com>
2023-03-31 11:00:16 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * rot13 - Entry Function
+ * @c: char
+ * Return: c (SUCCESS)
+ */
+char *rot13(char *c)
+{
+	if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+		c += 13;
+	else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+		c -= 13;
+	return (c);
+}
