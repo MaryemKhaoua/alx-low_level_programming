d8b0899b8d67ae0fc41f1bbb862c119d3e80d24f
Maryem <maryem.khaoua@gmail.com>
2023-04-05 02:02:10 -0700

    Inception Is it possible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,7 +20,7 @@ int _prime(int n, int i)
 {
 	if (i >= n && n >= 2)
 		return (1);
-	else if (n % i == 0 || n <= 0)
+	else if (n % i == 0 || n < 2)
 		return (0);
 	else
 		return (_prime(n, i + 1));
