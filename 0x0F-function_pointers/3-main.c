445452e7b5fe16243641b6e363a2f39ba1a6b4e4
Maryem <maryem.khaoua@gmail.com>
2023-04-19 18:24:52 -0700

    Error fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,21 +8,31 @@
  * @argv: char
  * Return: returns the result of the operation
  */
-
 int main(int argc, char *argv[])
 {
+	int nbr1, nbr2;
+	char *op;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (get_op_func(argv[2]))
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-	else
+	nbr1 = atoi(argv[1]);
+	op = argv[2];
+	nbr2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
+	if ((*op == '/' && nbr2 == 0) || (*op == '%' && nbr2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(nbr1, nbr2));
 	return (0);
 }
