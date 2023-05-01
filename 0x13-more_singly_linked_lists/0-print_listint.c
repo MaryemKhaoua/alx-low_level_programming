af845ece205f832d3677dfe3c4979104f244920b
Maryem khaoua <maryem.khaoua@gmail.com>
2023-04-30 17:28:17 -0700

    fix betty error

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * print_listint - Entry Function
- * @h: pointer
- * Retunr: the number of nodes
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: nbr
  */
 size_t print_listint(const listint_t *h)
 {
