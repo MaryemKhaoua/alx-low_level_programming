f1d9c75a108c89c31a22f8a312de07e68826a7f4
Maryem khaoua <maryem.khaoua@gmail.com>
2023-05-04 17:59:40 -0700

    error fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,23 +5,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int mym = 1;
+	unsigned long int mym = 1UL << ((sizeof(unsigned long int) * 8) - 1);
 	int mol_zraq = 0; /* don't mind this hahaha💙 💙 */
 
-	while ((mym << 1) <= n)
-		mym <<= 1;
+	if (n == 0)
+		_putchar('0');
 
-	while (mym != 0)
+	while (mym > 0)
 	{
-		if (n & mym)
+		if ((n & mym) == mym)
+		{
 			_putchar('1');
-		else
+			mol_zraq = 1;
+		}
+		else if (mol_zraq)
 			_putchar('0');
 
-		mym >>= 1;
-		mol_zraq++;
+		mym = mym >> 1;
 	}
 
-	if (mol_zraq == 0)
-		_putchar('0');
 }
