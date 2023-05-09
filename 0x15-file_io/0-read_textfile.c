35e9f32608cba6de7040cc0c2404b69a26a438af
Maryem khaoua <maryem.khaoua@gmail.com>
2023-05-08 17:00:35 -0700

    Tread lightly  she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	char *buff;
+	ssize_t nbytes;
+
+	if (filename == NULL)
+		return (0);
+	fptr = fopen(filename, "r");
+	if (fptr == NULL)
+		return (0);
+	buff = malloc(sizeof(char) * letters);
+	nbytes = fread(buff, sizeof(char), letters, fptr);
+	if (nbytes <= 0 || (size_t) fwrite(buff, sizeof(char),
+	nbytes, stdout) != (size_t) nbytes)
+	{
+		free(buff);
+		fclose(fptr);
+		return (0);
+	}
+	free(buff);
+	fclose(fptr);
+	return (nbytes);
+}
