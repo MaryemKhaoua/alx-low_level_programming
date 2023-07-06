85285f5bb07478a1daf6c5bc6fb6b17624cdedf6
Maryem khaoua <maryem.khaoua@gmail.com>
2023-07-06 08:51:41 -0700

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,15 +7,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
-
-	for (int i = 0; b[i] != '\0'; i++)
+	for (i = 0 ; b[i] != '\0' ; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] == '0')
+			res = res << 1;
+		else if (b[i] == '1')
+			res = (res << 1) | 1;
+		else
 			return (0);
-		res = (res << 1) | (b[i] - '0');
 	}
 	return (res);
 }
