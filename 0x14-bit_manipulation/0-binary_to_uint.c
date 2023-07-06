55009abe80fcde9f8ef2fcfc9eb8815ea8f39f16
Maryem khaoua <maryem.khaoua@gmail.com>
2023-07-06 08:48:16 -0700

    fixed errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		res = 2 * res + (b[i] - '0');
+		res = (res << 1) | (b[i] - '0');
 	}
 	return (res);
 }
